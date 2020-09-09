b978b30b768510d351a772cdaf1a3ea5a386f0d4
Diego Machado <machadodiegoh@gmail.com>
2020-09-09 00:20:11 -0300

    added stack navigation and application logo images

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+
+import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+    </Container>
+  );
+};
+
+export default SignIn;
